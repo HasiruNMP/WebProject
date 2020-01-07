084579051d7d337c6c4436c7a78d1c823ccc9b6d
Rakshitha <52496596+RakshithaDi@users.noreply.github.com>
2020-01-08 01:51:37 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,23 +1,45 @@
 <html>
 <head>
-	<title> </title>
-	<script src="javascript.js"></script>
-	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+	<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="users";
 
-	<?php ///connecting nsbmtshirts database
-	
-	$connection= mysqli_connect("localhost","root","");
-	$db=mysqli_select_db($connection,"nsbmtshirts"); //selecting the database
+$conn=mysqli_connect($servername,$username,$password,$dbname);
 
-	?>
+if($conn->connect_error)
+{
+echo("Connection failed:".$conn->connect_error);
+}
+else
+{
+echo("Connection Successful");
+}
 
-	
-	
+$firstname=$_REQUEST['firstname'];
+$lastname=$_REQUEST['lastname'];
+$username=$_REQUEST['username'];
+$password=$_REQUEST['password'];
+$gender=$_REQUEST['gender'];
+$year=$_REQUEST['year'];
 
 
 
+$query="INSERT INTO student (firstname, lastname, username, password, gender, year) VALUES ('$firstname', '$lastname', '$username', '$password', '$gender', '$year');";
+
+if($conn->query($query))
+{
+echo "Record Added successfully!";
+}
+else
+{
+echo "Error";
+}
+
+?>
 
 </body>
 </html>
