17e3ed490682602322bac6409bde4650262d01b8
Rakshitha <52496596+RakshithaDi@users.noreply.github.com>
2020-01-08 01:44:39 +0530

    php file for index page

---
+++ login.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title> </title>
+	<script src="javascript.js"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<?php ///connecting nsbmtshirts database
+	
+	$connection= mysqli_connect("localhost","root","");
+	$db=mysqli_select_db($connection,"nsbmtshirts"); //selecting the database
+
+	?>
+
+	
+	
+
+
+
+
+</body>
+</html>
