045555dcb6a98fe1b64098b516731419a8054f3d
Dinith Tennakoon <53987995+keshdinith@users.noreply.github.com>
2020-01-09 10:43:08 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,11 +2,22 @@ function home()
 	{
   		window.open("customerpage.html");
   	}
-  	function adminlogin() 
-	{
-  		window.open("adminlogin.html");
-  	}
-  	function logout() 
+
+function logout() 
 	{
   		window.open("index.html");
   	}
+    
+function validate() 
+    {
+      if (document.frm_1.uname.value!="admin")
+      {
+        alert("Incorrect username !!!");
+        document.getElementById('loginpage').action="js_validation.html";
+      }
+      if (document.frm_1.upassword.value!="admin")
+      {
+        alert("Incorrect password !!!");
+        document.getElementById('loginpage').action="js_validation.html";
+      }
+    }
