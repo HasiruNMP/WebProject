8f9c4854a1de7eb495cb54387d1c25b32fba5311
HasiruNMP <hasirunmp@gmail.com>
2020-01-13 13:46:59 +0530

    Update orders.php

--- orders.php
+++ orders.php
@@ -31,7 +31,7 @@
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
-            echo "Connected successfully";
+            //echo "Connected successfully";
         ?>
 
         <form method="get" action="#">
