6116d9e9c32164914d742855437d0d850985af32
Rakshitha <52496596+RakshithaDi@users.noreply.github.com>
2020-01-14 19:12:52 +0530

    Update storingorderdetails.php

--- storingorderdetails.php
+++ storingorderdetails.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Tshirt1 </title>
+	<link rel="shortcut icon" type="image/x-icon" href="Photos/logo1.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 
-<?php
+	<?php
 
 $connection= mysqli_connect("localhost","root","");
 $db=mysqli_select_db($connection,"nsbmtshirts"); //selecting the database
@@ -39,7 +43,48 @@ $query="INSERT INTO orders (Theme, StudentID, Gender, ContactNO, ProductID, Mate
 
 ?>
 
+
+
+	<table class="site">
+	<tr class="top">
+		<td class="head"><!--Name of the site-->
+			<img src="Photos/tshirtlogo.png"style="width:310px;height: auto;" >
+		</td>
+		<td class="logo"><!--logo class-->
+			<img src="Photos/nsbmlogo.png"style="width:310px;height: auto;" >
+		</td>
+	</tr>
+	
+	</table>
+
+	<div>
+	<table class="clearfix" align="center">
+      <tr>
+      	
+        <td><button class="button" style="vertical-align:middle" onclick="adminlogin()"><span>Admin Login</span></button></td>
+        <td><button class="button" style="vertical-align:middle" onclick="studentlogin()"><span>Log Out</span></button></td>
+        <td><button class="button" style="vertical-align:middle" onclick="window.location.href='#contact'"><span>Contact Us</span></button></td>
+      </tr>
+    </table>
+</div>
+	<hr>
+
+<div>
+
+
 <h1> Thankyour For ordering </h1>
+</div>
+
+
+
+<div id="contact" class="contact"><!--contacting options-->
+	<p ><u><center>Contact Us</center></u></p>
+	<p><center>tshirts@nsbm.com</center></p>
+	<p><center>011-1231235</center></p>
+</div>
+
+	
+
 
 </body>
 </html>
