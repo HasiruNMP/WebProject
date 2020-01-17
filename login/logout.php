8f1d65bef711d6cbf0645a5ffed6e9a5b2653811
keshdinith <tmdktennakoon@students.nsbm.lk>
2020-01-17 19:01:03 +0530

    Update logout.php

--- login/logout.php
+++ login/logout.php
@@ -2,7 +2,7 @@
    session_start();
    unset($_SESSION["username"]);
    unset($_SESSION["password"]);
-   unset($_SESSION['adminpw']);
+   
    
    //echo 'You have cleaned session';
    header("Location: login.php");
