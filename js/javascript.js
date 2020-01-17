0b01dda386e2ed499e57d81fdaa077e05d27d427
keshdinith <tmdktennakoon@students.nsbm.lk>
2020-01-17 19:01:23 +0530

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,6 @@
-function customerpage() 
-	{
-    this.close();
-  		window.open("index.php");
-  	}
-
-function studentlogin() 
-	{
-    this.close();
-  		window.open("login3.html");
-  	}
-
-function adminlogin() 
-  {
-      window.open("../admin/adminlogin.php");
-    }
-
-
 function goback() {
   this.close();
   window.history.back();
 }
 
-function orderpage(){
-  window.open("orders.php");
-}
+
