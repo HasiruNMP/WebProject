e51d820e596ed2727f9714226ead17ae5615fc87
keshdinith <tmdktennakoon@students.nsbm.lk>
2020-01-17 18:32:18 +0530

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -9,33 +9,12 @@ function studentlogin()
     this.close();
   		window.open("login3.html");
   	}
-    
-function validate() 
-    {
-      if (document.frm_1.uname.value!="admin")
-      {
-        alert("Incorrect username !!!");
-        document.getElementById('loginpage').action="js_validation.html";
-      }
-      if (document.frm_1.upassword.value!="admin")
-      {
-        alert("Incorrect password !!!");
-        document.getElementById('loginpage').action="js_validation.html";
-      }
-    }
 
 function adminlogin() 
   {
-    this.close();
-      window.open("js_validation.html");
+      window.open("../admin/adminlogin.php");
     }
 
-function adminlogin2() 
-{
-  this.close();
-    window.open("js_validation.html");
-  }
-  
 
 function goback() {
   this.close();
